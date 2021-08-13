011acc9af63865cc76c23cd290761ed4be4494b3
Anton Samokhvalov <anton@samokhvalov.xyz>
2021-08-13 20:06:03 +0300

    better

--- heirloom/libcommon/getdir.c
+++ heirloom/libcommon/getdir.c
@@ -1,28 +1,33 @@
 #include "getdir.h"
 
+#include <err.h>
 #include <dirent.h>
 #include <errno.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
 
 struct getdb* getdb_alloc(const char* path, int fd) {
     return (struct getdb*)fdopendir(dup(fd));
 }
 
 void getdb_free(struct getdb* db) {
-    closedir((DIR*)db);
+    if (closedir((DIR*)db)) {
+        err(-1, "closedir");
+    }
 }
 
-struct direc* getdir(struct getdb* db, int* err) {
+// intentional memory leak here
+struct direc* getdir(struct getdb* db, int* error) {
     struct dirent* ent;
     struct direc* res;
 
-    *err = 0;
-
+    *error = 0;
+    errno = 0;
     ent = readdir((DIR*)db);
 
     if (!ent) {
-        *err = errno;
+        *error = errno;
 
         return NULL;
     }
@@ -30,13 +35,15 @@ struct direc* getdir(struct getdb* db, int* err) {
     res = (struct direc*)malloc(sizeof(*res));
 
     if (!res) {
-        *err = ENOMEM;
-
-        return NULL;
+        err(-1, "malloc");
     }
 
     res->d_ino = ent->d_ino;
-    res->d_name = ent->d_name;
+    res->d_name = strdup(ent->d_name);
+
+    if (!res->d_name) {
+        err(-1, "strdup");
+    }
 
     return res;
 }
